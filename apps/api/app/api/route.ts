222a01bcc97e7d80bf27f29d8f82ba87dcee30a7
case <case@casesandberg.com>
2024-05-01 16:47:43 -0700

    Force API to V1

---
+++ apps/api/app/api/route.ts
@@ -0,0 +1,5 @@
+export const dynamic = 'force-dynamic'
+
+export function GET(request: Request) {
+  return new Response('Welcome to Play Money API')
+}
--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/v1/:path*',
+        destination: `/api/:path*`,
+      },
+    ]
+  },
 }
