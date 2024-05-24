9caeda9082fc5ea0924e093f4db8a3e3ce8e58f9
Tony <tonyedvalson@gmail.com>
2024-05-23 18:37:22 -0700

    Add seed and use uuid for now. Maybe switch to autoincrement later.

--- packages/database/seed.ts
+++ packages/database/seed.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker'
 import _ from 'lodash'
 import zod from 'zod'
 import db from './prisma'
-import { UserSchema } from './zod'
+import { UserSchema, MarketSchema } from './zod'
 
 function fakerUser(): zod.infer<typeof UserSchema> {
   const firstName = faker.person.firstName()
@@ -12,20 +12,44 @@ function fakerUser(): zod.infer<typeof UserSchema> {
     username: faker.internet.userName({ firstName, lastName }),
     displayName: faker.person.fullName({ firstName, lastName }),
     email: faker.internet.email({ firstName, lastName }),
-    avatarUrl: faker.helpers.maybe(faker.image.avatar),
-    bio: faker.helpers.maybe(faker.person.bio),
-    twitterHandle: faker.helpers.maybe(faker.internet.userName, { probability: 0.3 }),
-    discordHandle: faker.helpers.maybe(faker.internet.userName, { probability: 0.3 }),
-    website: faker.helpers.maybe(faker.internet.domainName, { probability: 0.3 }),
+    avatarUrl: faker.helpers.maybe(faker.image.avatar) ?? null,
+    bio: faker.helpers.maybe(faker.person.bio) ?? null,
+    twitterHandle: faker.helpers.maybe(faker.internet.userName, { probability: 0.3 }) ?? null,
+    discordHandle: faker.helpers.maybe(faker.internet.userName, { probability: 0.3 }) ?? null,
+    website: faker.helpers.maybe(faker.internet.domainName, { probability: 0.3 }) ?? null,
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+  }
+}
+
+function fakerMarket(userIds: string[]): zod.infer<typeof MarketSchema> {
+  const question = faker.lorem.sentence()
+  const description = faker.lorem.paragraph()
+  const closeDate = faker.date.future()
+  return {
+    id: faker.string.uuid(),
+    question,
+    description,
+    slug: faker.helpers.slugify(question),
+    closeDate,
+    resolvedAt: faker.helpers.maybe(faker.date.past, { probability: 0.3 }) ?? null,
+    createdBy: faker.helpers.arrayElement(userIds),
     createdAt: faker.date.past(),
     updatedAt: faker.date.recent(),
   }
 }
 
 async function main() {
-  await Promise.all(
+  let user_ids = await Promise.all(
     _.times(10, async () => {
-      await prisma.user.create({ data: fakerUser() })
+      let data = fakerUser()
+      await prisma.user.create({ data })
+      return data.id
+    })
+  )
+  await Promise.all(
+    _.times(5, async () => {
+      await prisma.market.create({ data: fakerMarket(user_ids) })
     })
   )
 }
