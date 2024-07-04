d85aab20b766acfe4e967d64ccce53e6d55007ae
case <case@casesandberg.com>
2024-07-04 16:54:08 -0700

    Init Storybook

---
+++ apps/storybook/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+import sharedConfig from '@play-money/config/tailwind/tailwind.config'
+
+const config: Pick<Config, 'content' | 'presets'> = {
+  content: ['./app/**/*.tsx', '../../packages/**/*.tsx'],
+  presets: [sharedConfig],
+}
+
+export default config
