6f53932bf659a7def03bd867bc2949080ff5bcce
case <case@casesandberg.com>
2024-07-07 13:39:40 -0700

    Add Footer

--- apps/web/app/(app)/layout.tsx
+++ apps/web/app/(app)/layout.tsx
@@ -89,6 +89,20 @@ export default async function AppLayout({ children }: { children: React.ReactNod
       </header>
 
       <div className="mx-auto flex max-w-screen-xl flex-1 space-y-4 p-4 md:p-8">{children}</div>
+      <div className="mx-auto flex gap-4 p-4 text-sm text-muted-foreground md:p-8">
+        <a className="hover:underline" href="https://discord.gg/3vgjSYT3" rel="noreferrer" target="_blank">
+          Join the discord
+        </a>
+        —
+        <a
+          className="hover:underline"
+          href="https://github.com/casesandberg/play-money"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Open source
+        </a>
+      </div>
     </div>
   )
 }
