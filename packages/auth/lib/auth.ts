3fd80bfac042710b54c7c5e40d6406eee5628dc0
case <case@casesandberg.com>
2024-07-06 21:17:28 -0700

    Remove Custom Cookie Support

--- packages/auth/lib/auth.ts
+++ packages/auth/lib/auth.ts
@@ -3,15 +3,6 @@ import Resend from 'next-auth/providers/resend'
 import db from '@play-money/database'
 import { PrismaAdapter } from './auth-prisma-adapter'
 
-if (!process.env.NEXTAUTH_URL) {
-  throw new Error('NEXTAUTH_URL is not set')
-}
-
-const useSecureCookies = process.env.NEXTAUTH_URL.startsWith('https://')
-const cookiePrefix = useSecureCookies ? '__Secure-' : ''
-const hostName = new URL(process.env.NEXTAUTH_URL).hostname
-const isVercel = hostName.includes('vercel.app')
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
   pages: {
@@ -21,20 +12,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   session: {
     maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-
-  // Support cookies on different subdomains
-  cookies: {
-    sessionToken: {
-      name: `${cookiePrefix}next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        domain: `.${isVercel ? 'vercel.app' : hostName}`,
-        secure: useSecureCookies,
-      },
-    },
   },
 
   providers: [
