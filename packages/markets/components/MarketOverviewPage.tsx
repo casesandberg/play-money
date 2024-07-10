57630c70c807268266b2a05d8942e324cdba059b
case <case@casesandberg.com>
2024-07-09 23:02:53 -0700

    Padding of market title

--- packages/markets/components/MarketOverviewPage.tsx
+++ packages/markets/components/MarketOverviewPage.tsx
@@ -63,7 +63,7 @@ export function MarketOverviewPage({
     <Card className="flex-1">
       <MarketToolbar market={market} canEdit={isCreator} onInitiateEdit={() => setIsEditing('true')} />
 
-      <CardHeader className="pt-0">
+      <CardHeader className="pt-0 md:pt-0">
         <CardTitle className="leading-relaxed">{market.question}</CardTitle>
         <div className="flex flex-row flex-wrap gap-x-4 gap-y-2 text-sm text-muted-foreground md:flex-nowrap">
           {!market.marketResolution ? <MarketLikelyOption market={market} /> : null}
