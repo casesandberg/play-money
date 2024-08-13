e2f4e4a349d1b98ef56af018c5c10140bb462604
case <case@casesandberg.com>
2024-08-13 11:56:25 -0700

    Fix Stories

--- packages/markets/components/MarketList.stories.tsx
+++ packages/markets/components/MarketList.stories.tsx
@@ -14,11 +14,36 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     markets: [
-      mockExtendedMarket(),
-      mockExtendedMarket(),
-      mockExtendedMarket(),
-      mockExtendedMarket(),
-      mockExtendedMarket(),
+      {
+        ...mockExtendedMarket(),
+        commentCount: 4,
+        liquidityCount: 4200,
+        uniqueTraderCount: 4,
+      },
+      {
+        ...mockExtendedMarket(),
+        commentCount: 0,
+        liquidityCount: 1000,
+        uniqueTraderCount: 0,
+      },
+      {
+        ...mockExtendedMarket(),
+        commentCount: 0,
+        liquidityCount: 1200,
+        uniqueTraderCount: 1,
+      },
+      {
+        ...mockExtendedMarket(),
+        commentCount: 2,
+        liquidityCount: 1400200,
+        uniqueTraderCount: 4124,
+      },
+      {
+        ...mockExtendedMarket(),
+        commentCount: 70,
+        liquidityCount: 1295,
+        uniqueTraderCount: 3,
+      },
     ],
   },
 }
@@ -26,35 +51,45 @@ export const Default: Story = {
 export const WithLongText: Story = {
   args: {
     markets: [
-      mockExtendedMarket({
-        question: faker.lorem.sentence(30),
-        options: [
-          mockMarketOption({ id: '1', name: faker.lorem.sentence(8), currencyCode: 'YES' }),
-          mockMarketOption({ id: '2', name: faker.lorem.sentence(8), currencyCode: 'NO' }),
-        ],
-        marketResolution: {
-          ...mockMarketResolution(),
-          resolution: mockMarketOption({
-            id: '2',
-            name: faker.lorem.sentence(8),
-            currencyCode: 'NO',
-            color: '#EC4899',
-          }),
-          resolvedBy: mockUser(),
-        },
-      }),
-      mockExtendedMarket({
-        question: faker.lorem.sentence(30),
-        options: [
-          mockMarketOption({ id: '1', name: faker.lorem.sentence(8), currencyCode: 'YES' }),
-          mockMarketOption({ id: '2', name: faker.lorem.sentence(8), currencyCode: 'NO' }),
-        ],
-        marketResolution: {
-          ...mockMarketResolution(),
-          resolution: mockMarketOption({ id: '2', name: faker.lorem.sentence(8), currencyCode: 'YES' }),
-          resolvedBy: mockUser(),
-        },
-      }),
+      {
+        ...mockExtendedMarket({
+          question: faker.lorem.sentence(30),
+          options: [
+            mockMarketOption({ id: '1', name: faker.lorem.sentence(8), currencyCode: 'YES' }),
+            mockMarketOption({ id: '2', name: faker.lorem.sentence(8), currencyCode: 'NO' }),
+          ],
+          marketResolution: {
+            ...mockMarketResolution(),
+            resolution: mockMarketOption({
+              id: '2',
+              name: faker.lorem.sentence(8),
+              currencyCode: 'NO',
+              color: '#EC4899',
+            }),
+            resolvedBy: mockUser(),
+          },
+        }),
+        commentCount: 4,
+        liquidityCount: 4200,
+        uniqueTraderCount: 4,
+      },
+      {
+        ...mockExtendedMarket({
+          question: faker.lorem.sentence(30),
+          options: [
+            mockMarketOption({ id: '1', name: faker.lorem.sentence(8), currencyCode: 'YES' }),
+            mockMarketOption({ id: '2', name: faker.lorem.sentence(8), currencyCode: 'NO' }),
+          ],
+          marketResolution: {
+            ...mockMarketResolution(),
+            resolution: mockMarketOption({ id: '2', name: faker.lorem.sentence(8), currencyCode: 'YES' }),
+            resolvedBy: mockUser(),
+          },
+        }),
+        commentCount: 0,
+        liquidityCount: 1000,
+        uniqueTraderCount: 0,
+      },
     ],
   },
 }
