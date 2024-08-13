3bf7f95ef9ddce59d43333c4fede2b3edde16128
case <case@casesandberg.com>
2024-08-12 17:04:40 -0700

    Net Worth Includes Other Income

--- packages/users/lib/getUserStats.ts
+++ packages/users/lib/getUserStats.ts
@@ -1,5 +1,6 @@
 import Decimal from 'decimal.js'
 import db from '@play-money/database'
+import { getMarketQuote } from '@play-money/markets/lib/getMarketQuote'
 import {
   hasPlacedMarketTradeToday,
   hasCreatedMarketToday,
@@ -69,6 +70,79 @@ async function getNetWorthByUser(userId: string) {
   return netWorth._sum.amount ?? new Decimal(0)
 }
 
+// NOTE: This is temporary as part of the financial re-write. Will be replaced with P&L statements.
+async function getOtherIncomeByUser(userId: string) {
+  const transactions = await db.transaction.findMany({
+    where: {
+      creator: {
+        userId,
+      },
+      type: { in: ['MARKET_BUY', 'MARKET_SELL'] },
+    },
+    include: {
+      transactionItems: true,
+    },
+  })
+
+  const allPositions = transactions.reduce(
+    (acc, transaction) => {
+      if (!transaction.marketId) {
+        return acc
+      }
+
+      const yesOptions = transaction.transactionItems.filter(
+        (item) => item.currencyCode === 'YES' && item.accountId === transaction.creatorId
+      )
+      const yesOptionsSum = yesOptions.reduce((itemSum, item) => itemSum.plus(item.amount), new Decimal(0))
+
+      const noOptions = transaction.transactionItems.filter(
+        (item) => item.currencyCode === 'NO' && item.accountId === transaction.creatorId
+      )
+      const noOptionsSum = noOptions.reduce((itemSum, item) => itemSum.plus(item.amount), new Decimal(0))
+
+      if (!acc[transaction.marketId] && (yesOptionsSum.greaterThan(0) || noOptionsSum.greaterThan(0))) {
+        acc[transaction.marketId] = { YES: 0, NO: 0 }
+      }
+
+      if (yesOptionsSum.greaterThan(0)) {
+        acc[transaction.marketId].YES = yesOptionsSum.plus(acc[transaction.marketId].YES).toNumber()
+      }
+
+      if (noOptionsSum.greaterThan(0)) {
+        acc[transaction.marketId].NO = noOptionsSum.plus(acc[transaction.marketId].NO).toNumber()
+      }
+
+      return acc
+    },
+    {} as Record<string, { YES: number; NO: number }>
+  )
+
+  let totalValue = new Decimal(0)
+
+  for (const [marketId, positions] of Object.entries(allPositions)) {
+    const yesMarketOption = await db.marketOption.findFirst({
+      where: { currencyCode: 'YES', marketId },
+    })
+    const noMarketOption = await db.marketOption.findFirst({
+      where: { currencyCode: 'NO', marketId },
+    })
+
+    for (const [currencyCode, amount] of Object.entries(positions)) {
+      if (amount > 0) {
+        const { shares: quote } = await getMarketQuote({
+          marketId,
+          optionId: (currencyCode === 'YES' ? yesMarketOption?.id : noMarketOption?.id) || '',
+          amount: new Decimal(amount),
+          isBuy: false,
+        })
+        totalValue = totalValue.plus(quote)
+      }
+    }
+  }
+
+  return totalValue.toNumber()
+}
+
 async function getLastTradeByUser(userId: string) {
   const lastTrade = await db.transactionItem.findFirst({
     where: {
@@ -95,6 +169,7 @@ export async function getUserStats({ userId }: { userId: string }) {
     hasCommented,
     hasBoostedLiquidity,
     activeDayCount,
+    otherIncome,
   ] = await Promise.all([
     getNetWorthByUser(userId),
     getTradingVolumeByUser(userId),
@@ -105,6 +180,7 @@ export async function getUserStats({ userId }: { userId: string }) {
     hasCommentedToday({ userId }),
     hasBoostedLiquidityToday({ userId }),
     calculateActiveDayCount({ userId }),
+    getOtherIncomeByUser(userId),
   ])
 
   return {
@@ -117,5 +193,6 @@ export async function getUserStats({ userId }: { userId: string }) {
     hasCommented,
     hasBoostedLiquidity,
     activeDayCount,
+    otherIncome,
   }
 }
