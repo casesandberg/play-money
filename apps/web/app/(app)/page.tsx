be7cd7193d0395d385965455301949c251ba4ad5
case <case@casesandberg.com>
2024-08-31 17:00:33 -0700

    Move Questions to Home Page

--- apps/web/app/(app)/page.tsx
+++ apps/web/app/(app)/page.tsx
@@ -1,5 +1,29 @@
-import { redirect } from 'next/navigation'
+import React from 'react'
+import { getMarkets } from '@play-money/api-helpers/client'
+import { RecentLiquidity } from '@play-money/finance/components/RecentLiquidity'
+import { RecentTrades } from '@play-money/finance/components/RecentTrades'
+import { MarketList } from '@play-money/markets/components/MarketList'
+import { UserQuestCard } from '@play-money/quests/components/UserQuestCard'
 
-export default function AppPage() {
-  redirect('/questions')
+export default async function AppPage() {
+  const { markets } = await getMarkets()
+
+  return (
+    <div className="mx-auto flex max-w-screen-lg flex-1 flex-col gap-8 md:flex-row">
+      <MarketList markets={markets} />
+
+      <div className="space-y-8 md:w-80">
+        <UserQuestCard />
+        <div>
+          <div className="pb-2 text-xs font-semibold uppercase text-muted-foreground">Recent trades</div>
+          <RecentTrades />
+        </div>
+
+        <div>
+          <div className="pb-2 text-xs font-semibold uppercase text-muted-foreground">New liquidity</div>
+          <RecentLiquidity />
+        </div>
+      </div>
+    </div>
+  )
 }
--- apps/web/app/(app)/questions/page.tsx
+++ apps/web/app/(app)/questions/page.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { getMarkets } from '@play-money/api-helpers/client'
-import { RecentLiquidity } from '@play-money/finance/components/RecentLiquidity'
-import { RecentTrades } from '@play-money/finance/components/RecentTrades'
 import { MarketList } from '@play-money/markets/components/MarketList'
-import { UserQuestCard } from '@play-money/quests/components/UserQuestCard'
 
 export default async function AppQuestionsPage() {
   const { markets } = await getMarkets()
@@ -11,19 +8,6 @@ export default async function AppQuestionsPage() {
   return (
     <div className="mx-auto flex max-w-screen-lg flex-1 flex-col gap-8 md:flex-row">
       <MarketList markets={markets} />
-
-      <div className="space-y-8 md:w-80">
-        <UserQuestCard />
-        <div>
-          <div className="pb-2 text-xs font-semibold uppercase text-muted-foreground">Recent trades</div>
-          <RecentTrades />
-        </div>
-
-        <div>
-          <div className="pb-2 text-xs font-semibold uppercase text-muted-foreground">New liquidity</div>
-          <RecentLiquidity />
-        </div>
-      </div>
     </div>
   )
 }
