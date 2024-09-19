0c74635ae98729c759b1efd4a6b5617d31295636
case <case@casesandberg.com>
2024-09-19 12:49:37 -0700

    Reduce random colors for initial market options

--- packages/markets/components/CreateMarketForm.tsx
+++ packages/markets/components/CreateMarketForm.tsx
@@ -31,27 +31,35 @@ import { clearPresistedData, getPersistedData, usePersistForm } from '../../ui/s
 
 const CREATE_MARKET_FORM_KEY = 'create-market-form'
 
-const COLORS = [
+let COLORS = [
   '#f44336',
-  '#e91e63',
   '#9c27b0',
-  '#673ab7',
   '#3f51b5',
   '#2196f3',
-  '#03a9f4',
-  '#00bcd4',
   '#009688',
-  '#4caf50',
   '#8bc34a',
-  '#cddc39',
-  '#ffeb3b',
   '#ffc107',
   '#ff9800',
-  '#ff5722',
   '#795548',
-  '#9e9e9e',
   '#607d8b',
 ]
+
+// This is a hack for Chromatic tests giving false positives.
+// TODO: Look into a seed based apprach or something that can be mocked.
+if (process.env.NODE_ENV === 'test') {
+  COLORS = [
+    '#f44336',
+    '#f44336',
+    '#f44336',
+    '#f44336',
+    '#f44336',
+    '#f44336',
+    '#f44336',
+    '#f44336',
+    '#f44336',
+    '#f44336',
+  ]
+}
 
 const marketCreateFormSchema = MarketSchema.pick({
   question: true,
