225c65d33402bf77eb96297802848a85188f020f
case <case@casesandberg.com>
2024-09-19 17:04:18 -0700

    Fix Ring Offsets in Dark Mode

--- packages/comments/components/CommentItem.tsx
+++ packages/comments/components/CommentItem.tsx
@@ -84,7 +84,7 @@ export function CommentItem({
     <div
       id={comment.id}
       className={cn(
-        isHighlighted && 'bg-primary/10 ring-2 ring-primary ring-offset-2 dark:ring-offset-black',
+        isHighlighted && 'bg-primary/10 ring-2 ring-primary ring-offset-2 ring-offset-background',
         'group flex flex-row gap-4 rounded-md px-6 py-2 hover:bg-muted/50',
         (isReplyOpen || isPortalOpen) && 'bg-muted/50'
       )}
