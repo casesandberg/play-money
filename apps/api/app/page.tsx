3af92f31058446326b1ffd2ab2182df821a0090f
case <case@casesandberg.com>
2024-11-27 14:39:59 -0800

    Swagger UI Components

--- apps/api/app/page.tsx
+++ apps/api/app/page.tsx
@@ -1,13 +1,9 @@
-import SwaggerUI from 'swagger-ui-react'
-import 'swagger-ui-react/swagger-ui.css'
-import docs from '../openapi.json'
-
-export const dynamic = 'force-dynamic'
+import { SwaggerUI } from './SwaggerUI'
 
 export default function IndexPage() {
   return (
     <section className="container">
-      <SwaggerUI spec={docs} />
+      <SwaggerUI />
     </section>
   )
 }
